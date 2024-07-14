8d801b3e2507efea37557e1ff5a88f353b65971e
jshalstr <joshaleisterv@gmail.com>
2024-07-14 15:27:10 +0800

    decrease gradient glow of GradientButton

--- components/GradientButton/GradientButton.tsx
+++ components/GradientButton/GradientButton.tsx
@@ -8,7 +8,7 @@ export default function GradientButton({ label, link, tooltip }: IProps) {
   return (
     <>
       <div className="relative inline-flex group">
-        <div className="absolute transitiona-all duration-1000 opacity-70 -inset-px bg-gradient-to-r from-[#F08F27] via-[#1DB9D1] to-[#691FE1] rounded-xl blur-lg group-hover:opacity-100 group-hover:-inset-1 group-hover:duration-200 animate-tilt"></div>
+        <div className="absolute transition-all duration-1000 opacity-70 inset-1 bg-gradient-to-r from-[#F08F27] via-[#1DB9D1] to-[#691FE1] rounded-xl blur-lg group-hover:opacity-100 group-hover:inset-0 group-hover:duration-200 animate-tilt"></div>
         <a
           href={link || '#'}
           title={tooltip}
