5a06e346827e493b7680e710865414ff01f11c36
jshalstr <joshaleisterv@gmail.com>
2024-07-18 20:59:26 +0800

    use NextImage as root element for Image

--- app/projects/page.tsx
+++ app/projects/page.tsx
@@ -16,6 +16,7 @@ import {
 import { useMediaQuery } from '@mantine/hooks';
 import { motion } from 'framer-motion';
 import { useState } from 'react';
+import NextImage from 'next/image';
 
 interface Project {
   title: string;
@@ -28,7 +29,7 @@ interface Project {
 const projects: Project[] = [
   {
     title: 'Glimpse',
-    image: 'images/glimpse.png',
+    image: '/images/glimpse.png',
     description: [
       'Tool for visually impaired students to communicate with teachers and engage in the classroom.',
       'Comprises three Android applications: smartwatch application for the blind, companion application for teachers, and interactive activity application for sighted students.',
@@ -40,7 +41,7 @@ const projects: Project[] = [
   },
   {
     title: 'Autopot',
-    image: 'images/autopot.jpg',
+    image: '/images/autopot.jpg',
     description: [
       'IoT system for automated plant maintenance.',
       'Monitors temperature, humidity, soil moisture, and light exposure.',
@@ -53,7 +54,7 @@ const projects: Project[] = [
   },
   {
     title: 'Logo Guesser',
-    image: 'images/logoguesser.jpg',
+    image: '/images/logoguesser.jpg',
     description: [
       'Engaging Android game challenging users to guess logos from various brands.',
       'Features a scoring system to track user progress.',
@@ -65,7 +66,7 @@ const projects: Project[] = [
   },
   {
     title: 'DejaBrew',
-    image: 'images/dejabrew.jpg',
+    image: '/images/dejabrew.jpg',
     description: [
       'Mock e-commerce website inspired by Starbucks.',
       'Allows users to add drinks to their cart and make purchases, accurately reflected in the database.',
@@ -77,7 +78,7 @@ const projects: Project[] = [
   },
   {
     title: 'Restaurant Ordering System',
-    image: 'images/ros.jpg',
+    image: '/images/ros.jpg',
     description: [
       'Console-based C# program using Binary Search Trees to create priority orders.',
       'Efficiently manages and prioritizes customer orders based on urgency.',
@@ -89,7 +90,7 @@ const projects: Project[] = [
   },
   {
     title: 'POS Express',
-    image: 'images/posexpress.jpg',
+    image: '/images/posexpress.jpg',
     description: [
       'Console-based C# program mimicking a point-of-sale system.',
       'Uses Object-Oriented Programming (OOP) principles.',
@@ -101,7 +102,7 @@ const projects: Project[] = [
   },
   {
     title: '4 Pics One Word Clone',
-    image: 'images/4pics.jpg',
+    image: '/images/4pics.jpg',
     description: [
       'Puzzle game where players guess the common word linking four images.',
       "Tests players' vocabulary and cognitive skills in a fun and challenging way.",
@@ -142,12 +143,13 @@ function ProjectDetails({ project }: { project: Project }) {
     >
       <Stack className="p-4 border rounded-lg shadow-lg m-auto">
         <Image
+          component={NextImage}
           src={project.image}
           alt={project.title}
           mah={250}
           maw={600}
           miw={{ base: 0, sm: 450 }}
-          w="auto"
+          width={600}
           height={300}
           fit="contain"
           radius="md"
